73d0f943305a5f0f045c01128b0a4281621cf942
tonyhengyi <929049939@qq.com>
2026-03-15 19:03:42 +0800

    feat: initial commit with sdl2 project

---
+++ SDL_Study/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <SDL.h>
+
+int main(int, char**) {
+    std::cout << "Hello, World!" << std::endl;
+    // SDL初始化
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    // 创建窗口
+    SDL_Window* window = SDL_CreateWindow("Hello World!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    // 创建渲染器
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // 渲染循环
+    while (true) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                break;
+            }
+        }
+
+        // 清屏
+        SDL_RenderClear(renderer);
+        // 画一个长方形
+        SDL_Rect rect = { 100, 100, 200, 200 };
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // 更新屏幕
+        SDL_RenderPresent(renderer);
+    }
+
+    // 清理并退出
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
